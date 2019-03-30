ca158941cfc754a43d63bc9dc9f44e2337341694
6kkubiak <49102470+6kkubiak@users.noreply.github.com>
2019-03-30 13:39:28 -0400

    Create Computer Info2.ps1

---
+++ Computer Info2.ps1
@@ -0,0 +1,51 @@
+﻿#Start Windows Remote Management
+Start-Service winrm
+
+#Computer Info
+$computerinfostart = ('===============Computer Information===============')
+$computerinfo = (Get-ComputerInfo -Property CsSystemFamily, WindowsProductName, CsSystemType, OsInstallDate, OsUptime, LogonServer, CsProcessors, CsNetworkAdapters)
+
+#Disk Info
+$diskinfostart = ('===============Disk Information===============')
+$diskinfo1 = (Get-WmiObject -Class Win32_LogicalDisk | Select-Object DeviceID, ProviderName, {$_.FreeSpace /1gb} | Format-List)
+ 
+#Processor Info
+$processorinfostart = ('===============Processor Info===============')
+$processor1 = (Get-WmiObject win32_processor | Measure-Object -property LoadPercentage -Average | Select Average)
+$processor2 = (get-wmiObject win32_processor | select loadpercentage)
+get
+#RAM Info
+$ramstart = ('===============RAM Info===============')
+$ram = (Get-WmiObject win32_physicalmemory | Select ConfiguredClockSpeed, {$_.Capacity /1gb})
+
+#IP Info
+$networkstart = ('===============Network Info===============')
+$ipaddress = (Get-NetIPAddress -InterfaceAlias Ethernet*, Wi-fi* | Select InterfaceAlias, IpAddress)
+
+$arr = @(
+#Computer Info
+$computerinfostart,
+$computerinfo,
+
+#Disk Info
+$diskinfostart,
+$diskinfo1,
+$diskinfo2,
+
+#Processor Info
+$processorinfostart,
+$processor1,
+$processor2,
+
+#RAM Info
+$ramstart,
+$ram,
+
+#Network Info
+$networkstart,
+$ipaddress
+)
+
+foreach ($item in $arr){
+    Write-Output ($item) >> "C:\Users\$($env:USERNAME)\Documents\SysInfo.txt"
+} 
